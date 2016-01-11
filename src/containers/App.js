8e1c7d97159e091504709d690e0cd59454e879f5
Juan Manuel Rodriguez <jmrodriguez@medallia.com>
2016-01-11 10:47:41 -0800

    containers added

---
+++ src/containers/App.js
@@ -0,0 +1,106 @@
+'use strict';
+
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux/native';
+import {Map} from 'immutable';
+import React, {} from 'react-native';
+import Login, {} from './Login';
+
+import * as authActions from '../reducers/auth/authActions';
+import * as deviceActions from '../reducers/device/deviceActions';
+import * as globalActions from '../reducers/global/globalActions';
+
+
+
+/**
+ * We only have one state to worry about
+ */
+
+const {
+  LOGIN_STATE_LOGOUT
+} = require('../lib/constants').default;
+
+/**
+ * ## Actions
+ * 3 of our actions will be available as ```actions```
+ */
+const actions = [
+  authActions,
+  deviceActions,
+  globalActions
+];
+
+/**
+ *  Save that state
+ */
+function mapStateToProps(state) {
+  return {
+      ...state
+  };
+};
+
+/*
+ * Bind all the functions from the ```actions``` and bind them with
+ * ```dispatch```
+
+ */
+function mapDispatchToProps(dispatch) {
+
+  const creators = Map()
+          .merge(...actions)
+          .filter(value => typeof value === 'function')
+          .toObject();
+
+  return {
+    actions: bindActionCreators(creators, dispatch),
+    dispatch
+  };
+}
+/**
+ * ## App class
+ */
+let App = React.createClass({
+  /**
+   * The initial state will be logged out
+   */
+  getInitialState() {
+    return {
+      loggedIn: false
+    };
+  },
+  /**
+   * Change the state to logged in if the Auth form is in the state of
+   * ```LOGIN_STATE_LOGOUT```.  This  state will be set after a
+   * successful Registrations or Login
+   */
+  componentWillReceiveProps(props) {
+    var loggedIn =  props.auth.form.state === LOGIN_STATE_LOGOUT;
+    this.setState({
+      loggedIn: loggedIn
+    });
+  },
+  /**
+   * See if there's a sessionToken from a previous login, if so, then
+   * the state will be changed to ```LOGIN_STATE_LOGOUT```
+   */
+  componentDidMount() {
+    this.props.actions.getSessionToken();
+  },
+  /**
+   * Display the ```Tabbar``` if we're logged in
+   */
+  render () {
+    let component = <Login/>;
+    if (this.state.loggedIn) {
+      component = <Tabbar/>;
+    }
+    return (
+      component
+    );
+  }
+});
+/**
+ * Connect the properties
+ */
+export default connect(mapStateToProps, mapDispatchToProps)(App);
+
---
+++ src/logJSON.js
@@ -0,0 +1,2 @@
+// For testing
+export default (json, msg='') => console.log(`${msg}[${typeof(json)}]: ${JSON.stringify(json)} \n`) ;
