0a8e428b93ae1de4ac93c100387330d755510e0d
7ynk3r <elmaildejuan@gmail.com>
2016-04-29 23:18:25 -0700

    SAVEPOINT

--- js/components/FreeBusyView.js
+++ js/components/FreeBusyView.js
@@ -5,21 +5,29 @@ import React, { View, ScrollView, Text } from 'react-native';
 import mock from '../__mocks__/googleapi.new';
 import XDate from 'xdate';
 
+const { freeBusy } = mock;
+
 export default class FreeBusyView extends React.Component {
   constructor() {
     super();
-    const timeMin = new XDate(mock.freeBusy.timeMin);
-    const timeMax = new XDate(mock.freeBusy.timeMax);
+
+    // hours
+    const timeMin = new XDate(freeBusy.timeMin);
+    const timeMax = new XDate(freeBusy.timeMax);
     var hours = [];
     for(var timeI=timeMin.clone(); timeI.diffMinutes(timeMax) >= 0; timeI.addHours(1)) {
       hours.push({'name':timeI.toString('h TT'), 'isLast':false});
     }
     hours.push({'name':timeI.toString('h TT'), 'isLast':true});
+
+    // busy
+    const cal = freeBusy.calendars["medallia.com_32343532323235353636@resource.calendar.google.com"];
+
     this.state = {
       hours
     }
   }
-  renderBlock(hour) {
+  renderHour(hour) {
     return (
       <View key={hour.name} style={{flex:1}}>
         <View style={{flex:1, height:20, flexDirection:'row', alignItems:'center'}}>
@@ -39,18 +47,21 @@ export default class FreeBusyView extends React.Component {
     );
   }
   render() {
-    const renderBlock = this.renderBlock;
     return (
-      <ScrollView
-        showsVerticalScrollIndicator={false}
-        automaticallyAdjustContentInsets={false}
-        horizontal={false}
-        vertical={true}
-        style={{flex:1, backgroundColor:'white', paddingLeft:15, paddingRight:15}}
-      >
-        <View style={{flex:1, height:50, flexDirection:'row'}}/>
-        { this.state.hours.map(renderBlock) }
-      </ScrollView>
+      <View style={{flex:1}}>
+        <View style={{backgroundColor:'gray',height:64}}/>
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          automaticallyAdjustContentInsets={false}
+          horizontal={false}
+          vertical={true}
+          style={{flex:1, backgroundColor:'white', paddingLeft:15, paddingRight:15}}
+        >
+          { this.state.hours.map(this.renderHour) }
+          <View style={{position:'absolute', top:0, left:55, right:0, bottom:0, backgroundColor:'yellow', opacity:.5}}>
+          </View>
+        </ScrollView>
+      </View>
     );
   }
 }
